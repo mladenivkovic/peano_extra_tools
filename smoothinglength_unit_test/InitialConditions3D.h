79a2be97a7169e85ecb26e5c9d5119865842a859
Mladen Ivkovic <mladen.ivkovic@hotmail.com>
2024-11-24 20:48:48 +0000

    fixed 3D unit tests

--- smoothinglength_unit_test/InitialConditions3D.h
+++ smoothinglength_unit_test/InitialConditions3D.h
@@ -5,11 +5,19 @@
 namespace initialConditions {
 
   extern struct InitialConditions ICUniform3D;
-  extern struct InitialConditions ICDisplaced3D;
-  extern struct InitialConditions ICMultilevelDisplaced3D;
+  extern struct InitialConditions ICDisplaced3D_30neighbours;
+  extern struct InitialConditions ICDisplaced3D_48neighbours;
+  extern struct InitialConditions ICDisplaced3D_64neighbours;
+  extern struct InitialConditions ICMultilevelDisplaced3D_30neighbours;
+  extern struct InitialConditions ICMultilevelDisplaced3D_48neighbours;
+  extern struct InitialConditions ICMultilevelDisplaced3D_64neighbours;
 
   struct InitialConditions getUniform3DIC(void);
-  struct InitialConditions getDisplaced3DIC(void);
-  struct InitialConditions getMultileveDisplaced3DIC(void);
+  struct InitialConditions getDisplaced3DIC_30neighbours(void);
+  struct InitialConditions getDisplaced3DIC_48neighbours(void);
+  struct InitialConditions getDisplaced3DIC_64neighbours(void);
+  struct InitialConditions getMultilevelDisplaced3DIC_30neighbours(void);
+  struct InitialConditions getMultilevelDisplaced3DIC_48neighbours(void);
+  struct InitialConditions getMultilevelDisplaced3DIC_64neighbours(void);
 
 } // namespace initialConditions
